664471ffe287b047a06ad2bd1c5bd65943182326
Prince Ninawe <princeninawe@microsoft.com>
2024-12-16 19:05:15 +0530

    modified github actions to use federated identity

--- terraform/subscriptions/subscription1/TF_Files/provider.tf
+++ terraform/subscriptions/subscription1/TF_Files/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azapi = {
+      source = "Azure/azapi"
+      version = "1.15.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
   storage_use_azuread = true
@@ -7,4 +16,8 @@ provider "azurerm" {
 provider "azuread" {
   use_oidc = true
 
+}
+
+provider "azapi" {
+  use_oidc = true
 }
